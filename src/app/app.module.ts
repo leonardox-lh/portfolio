d4f25f8c23f3ed3e4dffabca9f55415d3e0c5861
leonardo <u202124304@upc.edu.pe>
2024-10-11 19:50:47 -0500

    add nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+  animations: [
+    trigger('enterState', [
+      state('void', style({
+        transform: 'translateY(-100%)',
+        opacity: 0
+      })),
+      transition(':enter',[
+        animate(600,style({
+          transform: 'translateY(0)',
+          opacity: 1
+        }))
+      ]),
+    ]),
+  ],
+})
+export class NavComponent {
+  isMenuOpen: boolean;
+  constructor() {
+    this.isMenuOpen = false;
+  }
+  toggleMenu(){
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
